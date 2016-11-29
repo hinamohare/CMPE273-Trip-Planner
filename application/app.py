ceb6bec01d2039fa0175b1f99641d744be8bc499
hinamohare <hina.mohare@outlook.com>
2016-11-28 23:28:39 -0800

    Lyft api class added

--- application/app.py
+++ application/app.py
@@ -10,7 +10,7 @@ from sqlalchemy import event
 from sqlalchemy import DDL
 import requests
 from flask import Response
-
+from lyft import LyftApi
 app = Flask(__name__) #define app using Flask
 
 
@@ -59,51 +59,6 @@ class LocationDetails(db.Model):
             'zip': self.zip,
             'coordinate': {'lat': self.lat,'lng': self.lng}
         }
-
-    # ***************************** lyft authorization and cost api*******************************
-class LyftApi:
-    @staticmethod
-    def getAccessToken():
-        """get Lyft access token for using lyft api"""
-
-        client_secrete = "WRU95RMFGN9kRV9VOIXhzBEaBcqwTzHV"
-        client_Id = "9M08-8z29d9G"
-        url = "https://api.lyft.com/oauth/token"
-        payload = {"grant_type": "client_credentials", "scope": "public"}
-        data = json.dumps(payload)
-
-        headers = {"Content-Type": "application/json"}
-        resp = requests.post(url, data=data, auth=(client_Id, client_secrete), headers=headers)
-        response = resp.content
-        response_json = json.loads(response)
-        access_token = response_json["access_token"]
-        return access_token
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def getLyftCost(start_lat, start_lng, end_lat, end_lng):
-        """   input: start_lat, start_lng, end_lat, end_lng
-                return:  name(Lyft), ride type (Lyft_line), cost(USD), time(minute), distance(miles)"""
-
-        lyft_cost_url = "https://api.lyft.com/v1/cost?start_lat=" + str(start_lat) + "&start_lng=" + str(start_lng) + "&end_lat=" + str(end_lat) + "&end_lng=" + str(end_lng)
-        access_token = LyftApi.getAccessToken()
-        mytoken = "bearer " + access_token
-        cost_resp = requests.get(lyft_cost_url, headers={'Authorization': mytoken})
-        cost_data = cost_resp.content
-        cost_json = json.loads(cost_data)
-        ride_data = cost_json["cost_estimates"][1]  # cheapest ride is Lyft_line
-        ride_type = ride_data["ride_type"]  # ride type
-        ride_time = ride_data["estimated_duration_seconds"] / 60  # time in minutes
-        ride_cost = ride_data["estimated_cost_cents_max"] / 100  # max cost in USD
-        ride_distance = ride_data["estimated_distance_miles"]  # distance in mile
-        lyft_cost_info = {"service_provider": "Lyft",
-                          "car_type": ride_type,
-                          "costs_by_cheapest_car_type": ride_cost,
-                          "duration": ride_time,
-                          "distance": ride_distance}
-        return lyft_cost_info
 
 #********************************************* functions ****************************************#
 start_point = {} # holds starting point name, lat and lng
@@ -237,6 +192,7 @@ def getPrice():
     global start_point
     global end_point
     input_json = request.get_json(force=True)
+    print input_json
 
     startlocation = request.json["startlocation"] # starting point of the travel
     location = startlocation.replace(" ","+")
@@ -280,7 +236,8 @@ def getPrice():
     final_result["providers"] = providers
 
     print final_result
-    return "success"
+    json_result = json.dumps(final_result)
+    return json_result
 
 
 
---
+++ application/lyft.py
@@ -0,0 +1,57 @@
+import urllib2
+
+from flask import Flask, render_template
+from flask import jsonify, request, session # import objects from the flask module
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
+from sqlalchemy import event
+from sqlalchemy import DDL
+import requests
+from flask import Response
+
+#***************************** lyft authorization and cost api*******************************
+class LyftApi:
+    @staticmethod
+    def getAccessToken():
+        #get Lyft access token for using lyft api
+
+        client_secrete = "WRU95RMFGN9kRV9VOIXhzBEaBcqwTzHV"
+        client_Id = "9M08-8z29d9G"
+        url = "https://api.lyft.com/oauth/token"
+        payload = {"grant_type": "client_credentials", "scope": "public"}
+        data = json.dumps(payload)
+
+        headers = {"Content-Type": "application/json"}
+        resp = requests.post(url, data=data, auth=(client_Id, client_secrete), headers=headers)
+        response = resp.content
+        response_json = json.loads(response)
+        access_token = response_json["access_token"]
+        return access_token
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def getLyftCost(start_lat, start_lng, end_lat, end_lng):
+        #input: start_lat, start_lng, end_lat, end_lng
+        #return:  name(Lyft), ride type (Lyft_line), cost(USD), time(minute), distance(miles)
+
+        lyft_cost_url = "https://api.lyft.com/v1/cost?start_lat=" + str(start_lat) + "&start_lng=" + str(start_lng) + "&end_lat=" + str(end_lat) + "&end_lng=" + str(end_lng)
+        access_token = LyftApi.getAccessToken()
+        mytoken = "bearer " + access_token
+        cost_resp = requests.get(lyft_cost_url, headers={'Authorization': mytoken})
+        cost_data = cost_resp.content
+        cost_json = json.loads(cost_data)
+        ride_data = cost_json["cost_estimates"][1]  # cheapest ride is Lyft_line
+        ride_type = ride_data["ride_type"]  # ride type
+        ride_time = ride_data["estimated_duration_seconds"] / 60  # time in minutes
+        ride_cost = ride_data["estimated_cost_cents_max"] / 100  # max cost in USD
+        ride_distance = ride_data["estimated_distance_miles"]  # distance in mile
+        lyft_cost_info = {"service_provider": "Lyft",
+                          "car_type": ride_type,
+                          "costs_by_cheapest_car_type": ride_cost,
+                          "duration": ride_time,
+                          "distance": ride_distance}
+        return lyft_cost_info
+
