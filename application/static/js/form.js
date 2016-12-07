b22a9920839621712203b3f8b32f2e4b7a230470
natashadsouza <natasha.dsouza2702@gmail.com>
2016-12-06 23:45:37 -0800

    Update form.js

--- application/static/js/form.js
+++ application/static/js/form.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 			
 			var inputdata = getpayloaddata();
 			
-			console.log(inputdata);
+			//console.log(inputdata);
             $.ajax({
                 url: '/result', // url where to submit the request
                 type : "post", // type of action post || get
@@ -12,9 +12,40 @@ $(document).ready(function(){
                 datatype : 'json', // data type
                 data : inputdata, // post data || get data
                 success : function(result) {
+                console.log()
+                var obj = JSON.parse(result);
+                //alert(obj.start)
+
+                console.log("Start point is "+obj.start)
+                //document.getElementById('result').innerHTML = result;
+              var tbl=$("<table/>").attr("id","mytable");
+
+             // var result2 = $("<div/>").attr("id","heading");
+              var st="<h3>"+obj.start+"</h3>";
+              var en="<h3>"+obj.end+"</h3>";
+
+                $("#heading").append(st+en);
+
+                $("#results").append(tbl);
+
+                    for(var i=0;i<obj.providers.length;i++)
+                        {
+                             var tr="<tr>";
+                              var td1="<td>"+obj.providers[i]["car_type"]+"</td>";
+                              var td2="<td>"+obj.providers[i]["name"]+"</td>";
+                              var td3="<td>"+obj.providers[i]["total_distance"]+"</td>";
+                              var td4="<td>"+obj.providers[i]["total_duration"]+"</td>";
+                              var td5="<td>"+obj.providers[i]["duration_unit"]+"</td>";
+                              var td6="<td>"+obj.providers[i]["distance_unit"]+"</td>";
+                              var td7="<td>"+obj.providers[i]["total_costs_by_cheapest_car_type"]+"</td>";
+                              var td8="<td>"+obj.providers[i]["currency_code"]+"</td></tr>";
+
+                            $("#mytable").append(tr+td1+td2+td3+td4+td5+td6+td7+td8);
+
+                        }
                     // you can see the result from the console
                     // tab of the developer tools
-                    console.log(result);
+                  //  console.log(result);
                 },
                 error: function(xhr, resp, text) {
                     console.log(xhr, resp, text);
